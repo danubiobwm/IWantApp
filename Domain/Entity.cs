01107bc268094785e20a96f11d2b0b142fbef925
Danubio de Araujo <danubio.bwm@gmail.com>
2024-04-28 22:43:22 -0300

    DbConnection

---
+++ Domain/Entity.cs
@@ -0,0 +1,17 @@
+﻿using IWantApp.Domain.Products;
+
+namespace IWantApp.Domain;
+
+public abstract class Entity
+{
+    public Entity()
+    {
+        Id = Guid.NewGuid();
+    }
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedBy { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public string EditedBy { get; set; }
+    public DateTime EditedOn { get; set; }
+}
